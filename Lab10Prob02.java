bf9216fc1875f8ac90a931cfd70ac7f07f59ece5
DrakeCole <149799229+DrakeCole@users.noreply.github.com>
2023-11-17 16:04:39 -0500

    Add files via upload

---
+++ Lab10Prob02.java
@@ -0,0 +1,62 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.EOFException;
+import java.util.Collections;
+
+/**
+ * File: Lab10Prob01.java
+ * Class: CSCI 1302
+ * Created by: Drake Cole and Bao Pham
+ * Created on: Nov 17, 2023
+ * Last Modified: Nov 17, 2023
+ * Description: translate a binary file into a program
+ */
+public class Lab10Prob02 {
+	public static void main(String[] args) throws FileNotFoundException, EOFException {
+		ArrayList <Person> people = new ArrayList<>();
+		
+		try(
+				DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));
+				DataOutputStream output = new DataOutputStream(new FileOutputStream("src/people-copy.dat"));){
+			{
+				
+				while(true) {
+					
+					int age = input.readInt();
+					String name = input.readUTF();
+					String address = input.readUTF();
+					int zip = input.readInt();
+					double salary = input.readDouble();
+					 	
+					output.writeInt(age);
+					output.writeUTF(name);
+					output.writeUTF(address);
+					output.writeInt(zip);
+					output.writeDouble(salary);
+					
+					people.add(new Person(age, name,  address, zip, salary));
+
+
+//					System.out.printf(("%s %d %s %d %.2f%n")
+//							,name, age, address, zip, salary);
+				}
+			}}catch(EOFException ex ) {
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+			}
+		Collections.sort(people);
+		for(int i = 0; i < people.size(); i++) {
+			System.out.println(people.get(i).toString());
+		}
+	}
+}
+
+
+
--- Person.java
+++ Person.java
@@ -1,9 +1,68 @@
 
-public class Person implements Comparable{
+public class Person implements Comparable<Person>{
 	private int age;
 	private String name = "";
 	private String address = "";
 	private int zip;
 	private double salary;
+	
+	public Person() {
+		
+	}
+	public Person(int age, String name, String address, int zip, double salary) {
+		setAge(age);
+		setName(name);
+		setAddress(address);
+		setZip(zip);
+		setSalary(salary);
+	}
+	
+	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public int compareTo(Person o) {
+		if(this.getSalary() < o.getSalary()) {
+			return 1;
+		} else if(this.getSalary() > o.getSalary()) {
+			return -1;
+		}else {
+			return 0;
+		}
+	}
+	@Override
+	public String toString() {
+		return "Person [age=" + age + ", name=" + name + ", address=" + address + ", zip=" + zip + ", salary=" + salary
+				+ "]";
+	}
 
 }
