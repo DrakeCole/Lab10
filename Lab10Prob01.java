974d765fc7160649b18d3bea7e3ebee5d2b5af2b
DrakeCole <149799229+DrakeCole@users.noreply.github.com>
2023-11-17 14:04:41 -0500

    Add files via upload

---
+++ Lab10Prob01.java
@@ -0,0 +1,15 @@
+/**
+ * File: Lab10Prob01.java
+ * Class: CSCI 1302
+ * Created by: Drake Cole and Bao Pham
+ * Created on: Nov 17, 2023
+ * Last Modified: Nov 17, 2023
+ * Description: translate a binary file into a program
+ */
+public class Lab10Prob01 {
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
