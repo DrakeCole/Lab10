c1278f922f88fe94faffca775e77c99cce0fb8b7
Bao Pham <90734895+baoga313@users.noreply.github.com>
2023-11-17 14:34:53 -0500

    Update Lab10Prob01.java
    test to read the file

--- Lab10Prob01.java
+++ Lab10Prob01.java
@@ -1,3 +1,11 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * File: Lab10Prob01.java
  * Class: CSCI 1302
@@ -7,9 +15,16 @@
  * Description: translate a binary file into a program
  */
 public class Lab10Prob01 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		try (
+				DataInputStream input = new DataInputStream(new FileInputStream("people.dat"));) {
+			int age;
+			while (true) {
+				age = input.readInt();
+				System.out.println(age);
+			}
+		} catch (EOFException ex) {
+			System.out.println("Reached the end of file");
+		}
 	}
-	
-
 }
